e50c418c2263f7166ab134493ee05fdcb2a628b3
agelako <agel_thanos@yahoo.gr>
2023-02-07 15:19:33 +0200

    Refactored finalizeHandler, more readable

--- finalize/finalizeHandler.js
+++ finalize/finalizeHandler.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
     $("#finalize_button").click(function () {
-        // console.log($(this).data);
         const courseId = $(this).data("courseId");
-        var jsonData = $(this).data("grades");
-        console.log(jsonData);
+        const jsonData = $(this).data("grades");
+
         Swal.fire({
             title: 'Are you sure you want to finalize?',
             text: "",
@@ -15,49 +14,54 @@ $(document).ready(function () {
             cancelButtonText: 'Cancel'
         }).then((result) => {
             if (result.value) {
-                // Perform the action for "Yes" here
-
                 $.ajax({
                     type: "POST",
                     url: "handleFinalize.php",
-                    data: {id: courseId},
-                    success: function(response) {
-                        var csvData = ["userid", "courseid", "time"].join(",");
-                        csvData += "\n";
-                        csvData += [jsonData[0].userid, jsonData[0].courseid, jsonData[0].time].join(",");
-                        csvData += "\n";
-                        csvData += ["username", "activity_name", "grade"].join(",");
-                        csvData += "\n";
-                        csvData += jsonData[0].grades.map(function(grade) {
-                            return [grade.username, grade.activity_name, grade.rawgrade].join(",");
-                        }).join("\n");
-                        var blob = new Blob([csvData],{type: "text/csv"});
-                        var link = document.createElement("a");
-                        link.href = URL.createObjectURL(blob);
-                        link.download = "grade_data.csv";
-                        link.click();
-                        console.log("The button was pressed and the request was successful.");
-                        require(['core/notification'], function(notification) {
-                            notification.addNotification({
-                                message: "Grades have been exported successfully!",
-                                type: "success"
-                            });
-                        });
-
-                    },
-                    error: function(xhr, status, error) {
-                        console.error("An error occurred: " + error);
-                        require(['core/notification'], function(notification) {
-                            notification.addNotification({
-                                message: "Something went wrong",
-                                type: "error"
-                            });
-                        });
-                    }
-
+                    data: { id: courseId },
+                    success: handleSuccess,
+                    error: handleError
                 });
-
             }
         });
+
+        function handleSuccess(response) {
+            const csvData = formatCSVData(jsonData);
+            downloadCSV(csvData);
+            displayNotification("Grades have been exported successfully!!", "success");
+        }
+
+        function handleError(xhr, status, error) {
+            console.error("An error occurred: " + error);
+            displayNotification("Something went wrong", "error");
+        }
+
+        function formatCSVData(jsonData) {
+            let csvData = ["userid", "courseid", "time"].join(",") + "\n";
+            csvData += [jsonData[0].userid, jsonData[0].courseid, jsonData[0].time].join(",") + "\n";
+            csvData += ["username", "activity_name", "grade"].join(",") + "\n";
+            csvData += jsonData[0].grades.map(function(grade) {
+                return [grade.username, grade.activity_name, grade.rawgrade].join(",");
+            }).join("\n");
+
+            return csvData;
+        }
+
+        function downloadCSV(csvData) {
+            const blob = new Blob([csvData], { type: "text/csv" });
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = "grade_data.csv";
+            link.click();
+        }
+
+        function displayNotification(message, type) {
+            require(['core/notification'], function(notification) {
+                notification.addNotification({
+                    message: message,
+                    type: type
+                });
+            });
+        }
     });
 });
+
