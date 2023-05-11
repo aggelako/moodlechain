dfcb96b272ca1379cd1d866e16485a8ecfecd244
aggelako <agel_thanos@yahoo.gr>
2023-05-11 14:28:14 +0000

    Some test code for metamask extension

--- finalize/js/helpers/accessControlMetamask.js
+++ finalize/js/helpers/accessControlMetamask.js
@@ -1,4 +1,14 @@
 function isAuthed(walletKey) {
+
+    if (typeof window.ethereum !== 'undefined') {
+        console.log("hasbulla")
+        // window.ethereum.enable();
+        console.log(ethereum.request({ method: 'eth_requestAccounts' }))
+    }
+    else {
+        console.log(ethereum.request({ method: 'eth_requestAccounts' }))
+    }
+
     console.log("checking to auth key:", walletKey);
     return true;
 }
